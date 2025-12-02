c321b8c6b15330ff63d39377f391085b7754a694
Prozorov Nikita <avengertarvus@gmail.com>
2025-12-03 02:51:52 +0700

    Added tests for day1 and day2

---
+++ Day1Tests/GeneralTests.cs
@@ -0,0 +1,23 @@
+﻿using Day1;
+
+namespace Day1Tests;
+
+public class GeneralTests {
+    [Fact]
+    public void Solver_Solves_Correctly_For_Simple_Method() {
+        var input = Parser.GetInput().ToList();
+
+        var result = Solver.SimpleMethod(input);
+        
+        Assert.Equal(3, result);
+    }
+    
+    [Fact]
+    public void Solver_Solves_Correctly_For_Click_Method() {
+        var input = Parser.GetInput().ToList();
+
+        var result = Solver.ClickMethod(input);
+        
+        Assert.Equal(6, result);
+    }
+}
---
+++ Day2Tests/GeneralTests.cs
@@ -0,0 +1,23 @@
+﻿using Day2;
+
+namespace Day2Tests;
+
+public class GeneralTests {
+    [Fact]
+    public void Solver_Solves_Correctly_For_Two_Repetitions() {
+        var input = Parser.GetInput();
+        
+        var result = Solver.CountForTwoRepetitions(input);
+        
+        Assert.Equal(1227775554, result);
+    }
+    
+    [Fact]
+    public void Solver_Solves_Correctly_For_Any_Repetitions() {
+        var input = Parser.GetInput();
+        
+        var result = Solver.CountForAnyRepetitions(input);
+        
+        Assert.Equal(4174379265, result);
+    }
+}
