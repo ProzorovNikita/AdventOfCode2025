d2869daecba0e40aaccbb15a2355ae5e6de6a2fc
Prozorov Nikita <avengertarvus@gmail.com>
2025-12-04 15:39:55 +0700

    Added day4 solution

---
+++ src/Day4/Program.cs
@@ -0,0 +1,7 @@
+﻿using Day4;
+
+var countForOneRepeat = Solver.CountMovable(Parser.GetInput());
+var countForManyRepeat = Solver.CountMovable(Parser.GetInput(), true);
+
+Console.WriteLine($"count for one repeat: {countForOneRepeat}");
+Console.WriteLine($"count for many repeat: {countForManyRepeat}");
