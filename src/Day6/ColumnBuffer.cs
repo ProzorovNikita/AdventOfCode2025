04b0cf61054fa60391b226fbb119171426842092
Prozorov Nikita <avengertarvus@gmail.com>
2025-12-07 03:13:52 +0700

    Added day6 solution

---
+++ src/Day6/ColumnBuffer.cs
@@ -0,0 +1,18 @@
+﻿namespace Day6;
+
+public class ColumnBuffer {
+    public OperationType? Operation { get; set; }
+    private readonly List<long> _numbers = [];
+
+    public void Add(long number) {
+        _numbers.Add(number);
+    }
+
+    public long Count() {
+        return Operation switch {
+            OperationType.Add => _numbers.Sum(),
+            OperationType.Multiply => _numbers.Aggregate((a, b) => a * b),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+}
